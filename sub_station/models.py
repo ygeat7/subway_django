40e2782c0537c7964d428891d0f5940448009f06
jake <ygeat7@gmail.com>
2023-06-15 17:41:34 +0900

    검색 페이지 구현

--- sub_station/models.py
+++ sub_station/models.py
@@ -30,3 +30,64 @@ class SubwayStation(models.Model):
 
     def __str__(self):
         return f'[지하철 : {self.stationid}] {self.stationname}'
+
+class SubTr(models.Model):
+    tr_date = models.TextField(blank=True, null=True)
+    tr_year = models.IntegerField(blank=True, null=True)
+    tr_mon = models.IntegerField(blank=True, null=True)
+    st_no = models.TextField(blank=True, null=True)
+    st_name = models.TextField(blank=True, null=True)
+    number_04_05_getin = models.IntegerField(db_column='04-05_GetIn', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters. Field renamed because it wasn't a valid Python identifier.
+    number_04_05_getout = models.IntegerField(db_column='04-05_GetOut', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters. Field renamed because it wasn't a valid Python identifier.
+    number_05_06_getin = models.IntegerField(db_column='05-06_GetIn', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters. Field renamed because it wasn't a valid Python identifier.
+    number_05_06_getout = models.IntegerField(db_column='05-06_GetOut', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters. Field renamed because it wasn't a valid Python identifier.
+    number_06_07_getin = models.IntegerField(db_column='06-07_GetIn', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters. Field renamed because it wasn't a valid Python identifier.
+    number_06_07_getout = models.IntegerField(db_column='06-07_GetOut', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters. Field renamed because it wasn't a valid Python identifier.
+    number_07_08_getin = models.IntegerField(db_column='07-08_GetIn', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters. Field renamed because it wasn't a valid Python identifier.
+    number_07_08_getout = models.IntegerField(db_column='07-08_GetOut', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters. Field renamed because it wasn't a valid Python identifier.
+    number_08_09_getin = models.IntegerField(db_column='08-09_GetIn', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters. Field renamed because it wasn't a valid Python identifier.
+    number_08_09_getout = models.IntegerField(db_column='08-09_GetOut', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters. Field renamed because it wasn't a valid Python identifier.
+    number_09_10_getin = models.IntegerField(db_column='09-10_GetIn', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters. Field renamed because it wasn't a valid Python identifier.
+    number_09_10_getout = models.IntegerField(db_column='09-10_GetOut', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters. Field renamed because it wasn't a valid Python identifier.
+    number_10_11_getin = models.IntegerField(db_column='10-11_GetIn', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters. Field renamed because it wasn't a valid Python identifier.
+    number_10_11_getout = models.IntegerField(db_column='10-11_GetOut', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters. Field renamed because it wasn't a valid Python identifier.
+    number_11_12_getin = models.IntegerField(db_column='11-12_GetIn', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters. Field renamed because it wasn't a valid Python identifier.
+    number_11_12_getout = models.IntegerField(db_column='11-12_GetOut', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters. Field renamed because it wasn't a valid Python identifier.
+    number_12_13_getin = models.IntegerField(db_column='12-13_GetIn', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters. Field renamed because it wasn't a valid Python identifier.
+    number_12_13_getout = models.IntegerField(db_column='12-13_GetOut', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters. Field renamed because it wasn't a valid Python identifier.
+    number_13_14_getin = models.IntegerField(db_column='13-14_GetIn', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters. Field renamed because it wasn't a valid Python identifier.
+    number_13_14_getout = models.IntegerField(db_column='13-14_GetOut', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters. Field renamed because it wasn't a valid Python identifier.
+    number_14_15_getin = models.IntegerField(db_column='14-15_GetIn', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters. Field renamed because it wasn't a valid Python identifier.
+    number_14_15_getout = models.IntegerField(db_column='14-15_GetOut', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters. Field renamed because it wasn't a valid Python identifier.
+    number_15_16_getin = models.IntegerField(db_column='15-16_GetIn', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters. Field renamed because it wasn't a valid Python identifier.
+    number_15_16_getout = models.IntegerField(db_column='15-16_GetOut', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters. Field renamed because it wasn't a valid Python identifier.
+    number_16_17_getin = models.IntegerField(db_column='16-17_GetIn', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters. Field renamed because it wasn't a valid Python identifier.
+    number_16_17_getout = models.IntegerField(db_column='16-17_GetOut', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters. Field renamed because it wasn't a valid Python identifier.
+    number_17_18_getin = models.IntegerField(db_column='17-18_GetIn', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters. Field renamed because it wasn't a valid Python identifier.
+    number_17_18_getout = models.IntegerField(db_column='17-18_GetOut', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters. Field renamed because it wasn't a valid Python identifier.
+    number_18_19_getin = models.IntegerField(db_column='18-19_GetIn', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters. Field renamed because it wasn't a valid Python identifier.
+    number_18_19_getout = models.IntegerField(db_column='18-19_GetOut', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters. Field renamed because it wasn't a valid Python identifier.
+    number_19_20_getin = models.IntegerField(db_column='19-20_GetIn', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters. Field renamed because it wasn't a valid Python identifier.
+    number_19_20_getout = models.IntegerField(db_column='19-20_GetOut', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters. Field renamed because it wasn't a valid Python identifier.
+    number_20_21_getin = models.IntegerField(db_column='20-21_GetIn', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters. Field renamed because it wasn't a valid Python identifier.
+    number_20_21_getout = models.IntegerField(db_column='20-21_GetOut', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters. Field renamed because it wasn't a valid Python identifier.
+    number_21_22_getin = models.IntegerField(db_column='21-22_GetIn', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters. Field renamed because it wasn't a valid Python identifier.
+    number_21_22_getout = models.IntegerField(db_column='21-22_GetOut', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters. Field renamed because it wasn't a valid Python identifier.
+    number_22_23_getin = models.IntegerField(db_column='22-23_GetIn', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters. Field renamed because it wasn't a valid Python identifier.
+    number_22_23_getout = models.IntegerField(db_column='22-23_GetOut', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters. Field renamed because it wasn't a valid Python identifier.
+    number_23_24_getin = models.IntegerField(db_column='23-24_GetIn', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters. Field renamed because it wasn't a valid Python identifier.
+    number_23_24_getout = models.IntegerField(db_column='23-24_GetOut', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters. Field renamed because it wasn't a valid Python identifier.
+    number_00_01_getin = models.IntegerField(db_column='00-01_GetIn', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters. Field renamed because it wasn't a valid Python identifier.
+    number_00_01_getout = models.IntegerField(db_column='00-01_GetOut', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters. Field renamed because it wasn't a valid Python identifier.
+    number_01_02_getin = models.IntegerField(db_column='01-02_GetIn', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters. Field renamed because it wasn't a valid Python identifier.
+    number_01_02_getout = models.IntegerField(db_column='01-02_GetOut', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters. Field renamed because it wasn't a valid Python identifier.
+    number_02_03_getin = models.IntegerField(db_column='02-03_GetIn', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters. Field renamed because it wasn't a valid Python identifier.
+    number_02_03_getout = models.IntegerField(db_column='02-03_GetOut', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters. Field renamed because it wasn't a valid Python identifier.
+    number_03_04_getin = models.IntegerField(db_column='03-04_GetIn', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters. Field renamed because it wasn't a valid Python identifier.
+    number_03_04_getout = models.IntegerField(db_column='03-04_GetOut', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters. Field renamed because it wasn't a valid Python identifier.
+    getin_total = models.IntegerField(blank=True, null=True)
+    getout_total = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'sub_tr'
--- sub_station/views.py
+++ sub_station/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 import folium
-from .models import SubwayStation, BusStation
+from .models import SubwayStation, BusStation, SubTr
+import plotly.express as px
+import pandas as pd
+from datetime import datetime
 
 def index(request):
     return render(request, 'sub_station/index.html')
@@ -14,7 +17,7 @@ def sub_render(request, 역이름):
     # folium 객체 생성, Marker 추가
     figure = folium.Map(location=[lat, lon], zoom_start=17)
     folium.Marker(location=[lat, lon], popup=subway_st[0].stationname, icon=folium.Icon(color='red',icon='star')).add_to(figure)
-    folium.CircleMarker(location=[lat,lon], radius=250, color='red', fill_color='red').add_to(figure)
+    # folium.CircleMarker(location=[lat,lon], radius=250, color='red', fill_color='red').add_to(figure)
 
     # 해당 지하철역에 근접한 버스정류장 리스트 생성 및 folium 객체에 Marker 추가
     busst_list = [int(x) for x in subway_st[0].nearbusstno.split(',')]
@@ -25,11 +28,61 @@ def sub_render(request, 역이름):
 
         folium.Marker(location=[bus_lat, bus_lon], popup=bus_name).add_to(figure)
 
+    chart, message = sub_tr_plot(역이름)
+
     # folium 객체의 _repr_html() 메소드를 통해 html 출력할 수 있도록 객체화
     context = {
         'map': figure._repr_html_(),
         'subname':subway_st[0].stationname,
         'buscnt': int(subway_st[0].nearbusstcnt),
+        'chart': chart,
+        'message': message,
     }
 
     return render(request, 'sub_station/map.html', context)
+
+def sub_tr_plot(역이름):
+
+    # 지하철 역이름을 필터로 모델에서 해당 값 가져오기
+    queryset = SubTr.objects.filter(st_name=역이름)
+    sub_df = pd.DataFrame.from_records(queryset.values())
+
+    get_in_col = [x for x in sub_df.columns if '_getin' in x]
+    get_out_col = [x for x in sub_df.columns if '_getout' in x]
+
+    tmp1 = sub_df[get_in_col].mean()
+    tmp2 = sub_df[get_out_col].mean()
+    tmp1.index = [(x.split('_')[1]+'_'+x.split('_')[2]) for x in tmp1.index]
+    tmp2.index = [(x.split('_')[1]+'_'+x.split('_')[2]) for x in tmp2.index]
+    tmp_df = pd.DataFrame({'승차인원': tmp1, '하차인원': tmp2})
+
+    total_mean = (tmp1 + tmp2).mean()
+
+    now_hour = [x for x in tmp_df.index if str(datetime.now().hour) in x]
+    get_in_cong = tmp1.loc[now_hour].mean()
+    get_out_cong = tmp2.loc[now_hour].mean()
+
+    if (get_in_cong >= total_mean)|(get_out_cong >= total_mean):
+        message = '현재 해당역은 혼잡한 시간대 입니다.'
+    else:
+        message = '현재 해당역은 한산한 시간대 입니다.'
+
+    fig = px.line(tmp_df, x=tmp_df.index, y=['승차인원', '하차인원'])
+    fig.add_hline(y=total_mean, line=dict(color='red', dash='dash'), name='평균승하차인원수')
+    fig.update_layout(
+        height=500,  # 그래프의 높이 설정 (픽셀 단위)
+        width=800,  # 그래프의 너비 설정 (픽셀 단위)
+        # legend=dict(
+        #     x=0.9,  # 범례의 가로 위치 (0~1 사이의 값)
+        #     y=1.0,  # 범례의 세로 위치 (0~1 사이의 값)
+        #     xanchor='center',  # 범례 위치를 x축의 중앙에 맞춤
+        #     yanchor='top',  # 범례 위치를 y축의 상단에 맞춤
+        # ),
+        xaxis_title='시간대',
+        yaxis_title='인원수',
+        xaxis_tickangle=45,
+        title_font=dict(size=20),
+        paper_bgcolor='white'
+    )
+
+    return fig.to_html(), message
